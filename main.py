dbb445513d5e67a96cd518034f969ca6fcf94f97
Alan <alnnagorny@gmail.com>
2025-10-26 18:00:18 +0100

    feat: :sparkles: change loginPage function to loop until valid value is input
    Used a "try" and "except ValueError" approach to re-prompt the user on the login page if an invalid value is input.
    
    References #7

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ def main():
 def loginPage():
     print('HELLO, AND WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:')
     print('[1] Log in with existing user\n[2] Create new user\n[3] Exit program')
-    valg = int(input('> '))
-    if valg == 1:
-        loginUser()
-    if valg == 2:
-        createUser()
-    if valg == 3:
-        exit()
-    else:
-        
-        exit()
+    while True:
+        try:
+            valg = int(input('> '))
+            if valg == 1:
+                loginUser()
+                break
+            elif valg == 2:
+                createUser()
+                break
+            elif valg == 3:
+                exit()
+            else:
+                print('Invalid value. Try again.')
+        except ValueError:
+            print('Invalid input. Please enter a number.')
 
 username = 'admin'
 password = 'password'
