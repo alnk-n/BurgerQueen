d75f85321b7f6617a532936dbecd863a850c2ebd
Alan <alnnagorny@gmail.com>
2025-10-26 17:48:42 +0100

    feat: :sparkles: add test values and change input data type to int
    Change minor grammatical error. Add pass to currently unused functions. Add hardcoded username and password for testing purposes. Add stylization.
    
    Refereces #7

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ def main():
     loginPage()
 
 def loginPage():
-    print("HELLO, WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:")
+    print("HELLO, AND WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:")
     print("[1] Log in with existing user\n[2] Create new user\n[3] Exit program")
-    valg = str(input())
+    valg = int(input("> "))
     if valg == 1:
         loginUser()
     if valg == 2:
         createUser()
     if valg == 3:
         exit()
+    else:
+        
+        exit()
+
+username = "admin"
+password = "password"
+
+def loginUser():
+    print('-' *30)
+
+def createUser():
+    pass
 
 main()
