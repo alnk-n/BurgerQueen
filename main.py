ae1ca956465c4b0a6a4c9999ef83100ab94b9f71
Alan <alnnagorny@gmail.com>
2025-10-26 20:07:02 +0100

    feat: :fire: remove redundant userPointer function
    i figured it actually makes the code more unreadable! also passed con and cursor objects in most of the code. almost don.

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ def main():
 
     con.close()
 
-def userPointer():
-    return input('> ')
-
 def homePage(con, cursor):
     print('HELLO, AND WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:')
     print('[1] Log in with existing user\n[2] Create new user\n[3] Exit program')
     while True:
         try:
-            valg = int(userPointer())
+            valg = int(input('> '))
             if valg == 1:
                 loginUser(con, cursor)
                 break
@@ -39,36 +36,36 @@ def checkExistingUser(cursor, inputUsername):
     else:
         return False
 
-def loginUser():
-    print('-' *50)
-    print('Login with existing username.')
+# def loginUser(con, cursor):
+#     print('-' *50)
+#     print('Login with existing username.')
     
-    while True:
-        print("Enter your username: ")
-        inputUsername = userPointer()
-        if checkExistingUser(inputUsername):
-            break # Stops loop if provided username exists in database
-        else:
-            print("This user doesn't exist. Do you wish to create a new account? (y/N)")
-            confirmAccountCreation = userPointer()
-            if confirmAccountCreation.lower() == 'y':
-                createUser(inputUsername) # If username isn't in database, program asks whether to send over input to createUser function
-                return
-            else:
-                print('-' *50)
-                print("Try logging in with an existing username.\n")
+#     while True:
+#         print("Enter your username: ")
+#         inputUsername = input('> ')
+#         if checkExistingUser(cursor, inputUsername):
+#             break # Stops loop if provided username exists in database
+#         else:
+#             print("This user doesn't exist. Do you wish to create a new account? (y/N)")
+#             confirmAccountCreation = input('> ')
+#             if confirmAccountCreation.lower() == 'y':
+#                 createUser(inputUsername) # If username isn't in database, program asks whether to send over input to createUser function
+#                 return
+#             else:
+#                 print('-' *50)
+#                 print("Try logging in with an existing username.\n")
                 
-    print('Input password.')
-    inputPassword = userPointer()
+#     print('Input password.')
+#     inputPassword = input('> ')
 
-    if user_database[inputUsername] == inputPassword: # Checks if password matches username key in dictionary
-        print("Login successful!")
-    else:
-        print("Invalid username or password.")
-        loginUser()
+#     if user_database[inputUsername] == inputPassword: # Checks if password matches username key in dictionary
+#         print("Login successful!")
+#     else:
+#         print("Invalid username or password.")
+#         loginUser()
 
 
-def createUser(inputUsername=None):
+def createUser(con, cursor, inputUsername=None):
     print('-' *50)
     # Provides account name upon creation if sent from loginUser() function
     if inputUsername:
@@ -79,14 +76,14 @@ def createUser(inputUsername=None):
     # If no value is provided through inputUsername, ask for username
     if not inputUsername:
         print('Enter a username for your new account:')
-        inputUsername = userPointer()
+        inputUsername = input('> ')
 
-    if checkExistingUser(inputUsername):
+    if checkExistingUser(con, cursor, inputUsername):
         print('This username already exists. Please choose a different one.')
-        createUser()  # Retry if username already exists
+        createUser(con, cursor)  # Retry if username already exists
     else:
         print('Enter a password for your new account (none to cancel):')
-        inputPassword = userPointer()
+        inputPassword = input('> ')
         if inputPassword == "": # If nothing is input, return to homepage
             print('-' *50)
             homePage()
