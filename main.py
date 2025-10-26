08e0ee0f6a2c15fff86d6c04f3a4ff44438834e4
Alan <alnnagorny@gmail.com>
2025-10-26 17:54:19 +0100

    refactor: :art: replace all " with '

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     loginPage()
 
 def loginPage():
-    print("HELLO, AND WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:")
-    print("[1] Log in with existing user\n[2] Create new user\n[3] Exit program")
-    valg = int(input("> "))
+    print('HELLO, AND WELCOME TO BURGER QUEEN. CHOOSE AN ALTERNATIVE:')
+    print('[1] Log in with existing user\n[2] Create new user\n[3] Exit program')
+    valg = int(input('> '))
     if valg == 1:
         loginUser()
     if valg == 2:
@@ -15,8 +15,8 @@ def loginPage():
         
         exit()
 
-username = "admin"
-password = "password"
+username = 'admin'
+password = 'password'
 
 def loginUser():
     print('-' *30)
