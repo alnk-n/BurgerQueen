1eb52e74a83047ca6f6e6f18a7e2d88fba56cf43
Alan <alnnagorny@gmail.com>
2025-10-29 23:32:26 +0100

    feat: :sparkles: add simple employee dashboard
    copied over similar function from customerDashboard(), and declared basic functions for all functionalities.
    
    Closes #35

--- src/dashboards.py
+++ src/dashboards.py
@@ -35,13 +35,36 @@ def redirectUserDashboard(con, cursor, username):
         customerDashboard(con, cursor, username, 'Login successful.')
 
 
-def employeeDashboard(con, cursor, username):
-    pass
+def employeeDashboard(con, cursor, username, exceptionMessage = None):
+    print('\n'*20)
+    if exceptionMessage:
+        print(exceptionMessage)
+    print('-' *50)
+    print(f'Hello {username}! Choose an option:')
+    print('[1] View ongoing orders\n[2] View all orders\n[3] See ingredient inventory\n[4] Log out')
+    while True:
+        try:
+            choice = int(input('> '))
+            if choice == 1:
+                orders.viewOngoingOrders(con, cursor)
+            elif choice == 2:
+                orders.viewAllOrders(con, cursor)
+            elif choice == 3:
+                showIngredientInventory(con, cursor)
+            elif choice == 4:
+                print('\n'*10)
+                print('-'*50)
+                homePage(con, cursor)
+            else:
+                print('Invalid value. Select option 1, 2, or 3, or log out with 4.')
+        except ValueError:
+            print('Invalid input. Please enter a number.')
 
 
 def customerDashboard(con, cursor, username, exceptionMessage = None):
     print('\n'*20)
-    print(exceptionMessage)
+    if exceptionMessage:
+        print(exceptionMessage)
     print('-' *50)
     print(f'Hello {username}! Choose an option:')
     print('[1] Order food\n[2] See order status\n[3] Log out')
@@ -60,3 +83,7 @@ def customerDashboard(con, cursor, username, exceptionMessage = None):
                 print('Invalid value. Order food with [1], See order status with [2], and Log out with [3]')
         except ValueError:
             print('Invalid input. Please enter a number.')
+
+
+def showIngredientInventory(con, cursor):
+    pass
