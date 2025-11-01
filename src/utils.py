1588b74385b8e9330920b3a7b1c1717091e08e6c
alnk-n <alnnagorny@gmail.com>
2025-11-01 01:33:13 +0100

    docs: :memo: add comments to utils.py
    commented the purpose and argument use of the returnCheck() function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 # utils.py
 
+# Essentially checks whether the user has pressed Enter without inputting anything. In my program, this means you wish to cancel/return.
+# Arguments:
+# userInput: checks input provided in argument. (e.g. a password/username)
 def returnCheck(userInput):
     if userInput == "":
         return True
